d3dc348fd2b3c810404d51fa36770a85fce05e3f
Visionpro2025 <damilare900120@gmail.com>
2025-08-22 23:35:57 -0400

    Create app_min.py

---
+++ app_min.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(page_title="Smoke Test")
+st.title("✅ Despliegue OK")
+st.write("Si ves esta pantalla, la plataforma funciona. El problema está en tu app.py.")
